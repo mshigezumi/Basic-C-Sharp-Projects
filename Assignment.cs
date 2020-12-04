7da909db588244c8ced7d1884ecccee3a97f5241
mshigezumi <71560004+mshigezumi@users.noreply.github.com>
2020-12-04 02:52:48 -0800

    First Projects

---
+++ Assignment.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Assignment
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, we're \"accounting\" on you!");
+        Console.Read();
+    }
+}
