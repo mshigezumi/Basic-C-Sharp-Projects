68ff3102a6a24027ce57be7e967d321099517a31
mshigezumi <71560004+mshigezumi@users.noreply.github.com>
2020-12-05 02:53:28 -0800

    Assignment2

--- myConsoleProject.cs/Program.cs
+++ myConsoleProject.cs/Program.cs
@@ -10,6 +10,13 @@ namespace myConsoleProject.cs
             string name = Console.ReadLine();
             Console.WriteLine("Hello, " + name + "!");
             Console.Read();
+
+            Console.WriteLine("What is your favorite number?");
+            string numberString = Console.ReadLine();
+            int number = Convert.ToInt32(numberString);
+            int result = number + 5;
+            Console.WriteLine("Your favorite number plus 5 is: " + result);
+            Console.Read();
         }
     }
 }
