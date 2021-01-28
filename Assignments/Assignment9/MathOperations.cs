9bac4b4c5c592b391b1086236cdb3e3c515b3436
mshigezumi <71560004+mshigezumi@users.noreply.github.com>
2021-01-28 07:57:06 -0800

    Assignment9 Update

---
+++ Assignments/Assignment9/MathOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment9
+{
+    class MathOperations
+    {
+        public static int result1;
+        public static int result2;
+        public void Add(int x, int y)
+        {
+            x += 10;
+            Console.WriteLine(y);
+            result1 = x;
+            result2 = y;
+        }
+    }
+}
---
+++ Assignments/Assignment9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment9
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Input a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input a second number");
+            string input = Console.ReadLine();
+            int number2 = 0;
+            if (input != "")
+            {
+                number2 = Convert.ToInt32(input);
+            }
+            MathOperations math = new MathOperations();
+            math.Add(number, number2);
+            Console.WriteLine("--");
+            Console.WriteLine(MathOperations.result1);
+            Console.WriteLine(MathOperations.result2);
+            Console.Read();
+        }
+    }
+}
