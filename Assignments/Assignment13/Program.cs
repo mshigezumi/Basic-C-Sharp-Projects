56a2c7e2747d3d3566889534cc4901ee34f5e7a3
mshigezumi <71560004+mshigezumi@users.noreply.github.com>
2021-02-17 07:47:22 -0800

    Add Assignment 13

---
+++ Assignments/Assignment13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.firstName = "Sample";
+            emp.lastName = "Student";
+            emp.SayName();
+
+            IQuittable quit = new Employee();
+            quit.Quit();
+            Console.Read();
+        }
+    }
+}
