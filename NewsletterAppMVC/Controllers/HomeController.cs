8078f591f9cad789a518146808f758ff4178493b
mshigezumi <71560004+mshigezumi@users.noreply.github.com>
2021-03-07 10:39:48 -0800

    Add MVC Apps

--- NewsletterAppMVC/Controllers/HomeController.cs
+++ NewsletterAppMVC/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using NewsletterAppMVC.Models;
+using NewsletterAppMVC.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +12,8 @@ namespace NewsletterAppMVC.Controllers
 {
     public class HomeController : Controller
     {
+        //private string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newsletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
         public ActionResult Index()
         {
             return View();
@@ -21,21 +27,37 @@ namespace NewsletterAppMVC.Controllers
             }
             else
             {
+                using (NewsletterEntities db = new NewsletterEntities())
+                {
+                    var signup = new SignUp();
+                    signup.FirstName = firstName;
+                    signup.LastName = lastName;
+                    signup.EmailAddress = emailAddress;
+
+                    db.SignUps.Add(signup);
+                    db.SaveChanges(); //SqlException: Violation of PRIMARY KEY constraint 'PK_SignUps'. Cannot insert duplicate key in object 'dbo.SignUps'. The duplicate key value is (0). The statement has been terminated.
+                }
+                
+                //string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES (@FirstName, @LastName, @EmailAddress)";
+
+                //using (SqlConnection connection = new SqlConnection(connectionString))
+                //{
+                //    SqlCommand command = new SqlCommand(queryString, connection);
+                //    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                //    command.Parameters.Add("@LastName", SqlDbType.VarChar);
+                //    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+
+                //    command.Parameters["@FirstName"].Value = firstName;
+                //    command.Parameters["@LastName"].Value = lastName;
+                //    command.Parameters["@EmailAddress"].Value = emailAddress;
+
+                //    connection.Open();
+                //    command.ExecuteNonQuery(); //System.Data.SqlClient.SqlException: 'Cannot insert the value NULL into column 'Id', table 'Newsletter.dbo.SignUps'; column does not allow nulls. INSERT fails. The statement has been terminated.'
+                //    connection.Close();
+                //}
+
                 return View("Success");
             }
-        }
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
         }
     }
 }
